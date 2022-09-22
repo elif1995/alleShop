9071bcaf96c44dc7a871b086e353b93ce3c80302
edimmfs <e21211995@gmail.com>
2022-09-22 21:46:29 +0300

    added cart reducers with redux

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,10 +1,91 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import {Link, useLocation, useParams, useNavigate} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
+import Message from '../components/Message'
+import {addToCart, removeFromCart} from '../actions/cartAction';
+
 
 const CartScreen = () => {
+  const { id: productId } = useParams();
+  const navigate = useNavigate();
+  
+  const qty = new URLSearchParams(useLocation().search).get('qty');
+  const dispatch = useDispatch();
+
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart
+
+  console.log(cartItems)
+
+  useEffect(() => {
+    if(productId){
+      dispatch(addToCart(productId, qty))
+    }
+  },[dispatch, productId, qty])
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id))
+  }
+
+  const checkoutHandler = () => {
+    navigate('/login?redirect=shipping')
+  }
+
   return (
-    <div>
-      CartScreen
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>עגלת קניות</h1>
+        {cartItems.length === 0 ? <Message>עגלה ריקה<Link to='/'> חזור אחורה</Link></Message>:(
+          <ListGroup variant='flush'>
+        {cartItems.map((item) => (
+          <ListGroup.Item key={item.product}>
+            <Row>
+              <Col md={2}>
+                <Image src={item.image} alt={item.name} fluid rounded/>
+              </Col>
+              <Col md={3}>
+                <Link to={`/product/${item.product}`}>{item.name}</Link>
+              </Col>
+              <Col md={2}>
+                ${item.price}
+              </Col>
+              <Col md={2}>
+                <Form.Control className='form-select' as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1} >
+                            {x + 1}
+                          </option>
+                        ))}
+                </Form.Control>
+              </Col>
+              <Col md={2}>
+                <Button type="button" variant='light' onClick={() => removeFromCartHandler(item.product)}>
+                  <i className="fas fa-trash"></i>
+                </Button>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>סך הכל ({cartItems.reduce((acc,item) => acc + item.qty, 0)}) פריטים</h2>
+              ${cartItems.reduce((acc,item) => acc + item.qty * item.price, 0).toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item className="d-grid gap-2" >
+              <Button type='button' className="btn-block" disabled={cartItems.length === 0} onClick={checkoutHandler} >המשך לתשלום</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+      
+      
+    </Row>
   )
 }
 
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -10,7 +10,7 @@ import Message from '../components/Message';
 
 const ProductScreen = () => {
 
-  const [qty, setQty] = useState(0)
+  const [qty, setQty] = useState(1)
 
   const {id} = useParams();
 
